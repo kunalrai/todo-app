43a69ba0fa402db4ce25e368b974d46452fab7e2
kunalrai <ikunalrai@gmail.com>
2016-07-23 11:02:25 +0530

    initial

--- config/database.js
+++ config/database.js
@@ -1,4 +1,2 @@
 module.exports = {
-    remoteUrl:'mongodb://ikunalrai:mlab135@ds027145.mlab.com:27145/stencil-dev',
-    localUrl: 'mongodb://localhost:27017/stencil-dev'
 };
--- server.js
+++ server.js
@@ -9,9 +9,8 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===============================================================
-mongoose.connect(database.remoteUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
 
-app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
+
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
@@ -20,7 +19,7 @@ app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-M
 
 
 // routes ======================================================================
-require('./app/routes.js')(app);
+
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
