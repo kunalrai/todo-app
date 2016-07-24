cb91fd89540b8ab247f8645911ceefd1ed3e8709
kunalrai <ikunalrai@gmail.com>
2016-07-25 00:31:41 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,2 +1,5 @@
 module.exports = {
+remoteUrl:'mongodb://ikunalrai:mlab135@ds027145.mlab.com:27145/stencil-dev',
+    localUrl: 'mongodb://localhost:27017/stencil-dev',
+    modulusUrl: 'mongodb://ikunalrai:abc123@jello.modulusmongo.net:27017/roqo2xOb'
 };
