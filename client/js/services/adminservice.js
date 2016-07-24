98c38d1edfb1a90bcc2eec7c5e0a4d43d5a4a96c
kunalrai <ikunalrai@gmail.com>
2016-07-25 00:50:40 +0530

    bug fixes

--- client/js/services/adminservice.js
+++ client/js/services/adminservice.js
@@ -3,7 +3,7 @@
 
 	// super simple service
 	// each function returns a promise object 
-	.factory('Users', ['$http',function($http) {
+	.factory('adminService', ['$http',function($http) {
 		return {
 			get : function() {
 				return $http.get('/api/user');
